1c02667725f208cea5d93012c21a9dce0cc72975
Damian Janas <d.janas13@gmail.com>
2014-08-31 19:57:37 +0200

    Added: OCR manager template

---
+++ ReGraph/ReGraph.Shared/Models/OCR/OCRManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ReGraph.Models.OCR
+{
+    public class OCRManager
+    {
+		public string Recognize(WriteableBitmap image)
+		{
+			throw new NotImplementedException();
+		}
+    }
+}
